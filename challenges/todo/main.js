0d843815a477637b117bfed8c5306f7d1ccf26d4
Dygerydoo <caserofrancisco@gmail.com>
2017-05-25 14:02:27 +0200

    finish new instance actions

--- challenges/todo/main.js
+++ challenges/todo/main.js
@@ -1,63 +1,43 @@
+var setDate = new Date(),
+    locale = "es-ES",
+    dateOptions = { weekday: 'long', month: 'long', day: 'numeric' }
+    readableDate = setDate.toLocaleDateString(locale, dateOptions);
+
+var todayDate = readableDate.charAt(0).toUpperCase() + readableDate.slice(1);
+
 new Vue({
   el: '#app',
   data: {
-    selectedPriority: '',
-    priorityType: '',
-    priorityFilter: 'all',
-    priorities: [
-      { text: 'Sin prioridad', value: 0 },
-      { text: 'Baja', value: 1 },
-      { text: 'Media', value: 2 },
-      { text: 'Alta', value: 3 },
-    ],
+    todoTitle: 'Mi lista de tareas',
+    todoDate: todayDate,
+    editTitle: false,
     taskItem: '',
-    taskList: [
-      { name: 'Hacer la compra', priority: 3, priorityName: 'Alta' },
-      { name: 'Pasear al perro', priority: 2, priorityName: 'Media' },
-      { name: 'Llevar el coche al taller', priority: 1, priorityName: 'Baja' },
-      { name: 'Comprarme una camiseta nueva', priority: 0, priorityName: 'Sin prioridad' },
-    ],
+    taskList: ['Hacer la compra','Pasear al perro', 'Llevar el coche al taller', 'Comprarme una camiseta nueva'],
   },
 
   methods: {
-    getPriorityNames: function(event) {
-      var target = event.target;
-      this.priorityType = target.options[target.selectedIndex].innerHTML;
-      this.selectedPriority = parseInt(target.value);
-    },
     addTask: function() {
-      if(this.taskItem === '' || this.selectedPriority === '') {
+      if(this.taskItem === '') {
         alert('Debes escribir una tarea y seleccionar una prioridad');
       } else {
-        this.taskList.push({
-          name: this.taskItem,
-          priority: this.selectedPriority,
-          priorityName: this.nombrequesea,
-        })
+        this.taskList.push(this.taskItem);
         this.taskItem = '';
       }
+    },
+    setTitle: function(event) {
+      this.todoTitle = event.target.value;
+      this.editTitle = false;
     },
     removeTask: function (task) {
       this.taskList.splice(this.taskList.indexOf(task), 1)
     },
+    addTaskFocus: function() {
+      this.$refs.taskInput.focus();
+    }
   },
   computed: {
-    badgePriority: function() {
-      return this.filteredTasks.map(task => ({
-        grey: task.priority === 0,
-        blue: task.priority === 1,
-        orange: task.priority === 2,
-        red: task.priority === 3
-      }));
-    },
-    filteredTasks: function() {
-      return this.taskList.filter((task) => {
-        if(this.priorityFilter !== 'all') {
-          return task.priority === this.priorityFilter;
-        } else {
-          return task;
-        }
-      });
+    taskListEmpty: function() {
+      this.taskList.length === 0;
     }
   }
 })
