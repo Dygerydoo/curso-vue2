bca3daf415b33a991563c9d2c6cd35ae765d2a79
Dygerydoo <caserofrancisco@gmail.com>
2017-07-30 22:37:45 +0200

    named slots

---
+++ lessons/slots/named-slots/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <song-list>
+      <h1 slot="header" class="title is-1">Lista de canciones</h1>
+      <button class="button is-primary">Botón</button>
+    </song-list>
+  </div>
+</template>
+
+<script>
+import SongList from './components/SongList.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SongList,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ lessons/slots/named-slots/src/components/songList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <slot name="header"></slot>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Song Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="songNumber in 5">
+          <td>Song {{songNumber}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['playlist'],
+}
+</script>
+
+<style>
+.box {
+  width: 80%;
+  margin: 1em auto;
+}
+</style>
