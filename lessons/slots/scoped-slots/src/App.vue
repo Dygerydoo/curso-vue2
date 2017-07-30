da1bc6f63756394489c22cfffa0323b1fa714cbb
Dygerydoo <caserofrancisco@gmail.com>
2017-07-30 22:48:18 +0200

    Scoped slots

---
+++ lessons/slots/scoped-slots/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <song-list>
+      <h1 slot="header" class="title is-1">Lista de canciones</h1>
+      <template scope="props">
+        <p>{{props.textFromChild}}</p>
+      </template>
+    </song-list>
+  </div>
+</template>
+
+<script>
+import SongList from './components/SongList.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SongList,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ lessons/slots/scoped-slots/src/components/songList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box">
+    <slot name="header"></slot>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Song Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="songNumber in 5">
+          <td>Song {{songNumber}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <slot></slot>
+    <slot :textFromChild="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['playlist'],
+  data() {
+    return {
+      text: 'Hola desde el hijo',
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  width: 80%;
+  margin: 1em auto;
+}
+</style>
