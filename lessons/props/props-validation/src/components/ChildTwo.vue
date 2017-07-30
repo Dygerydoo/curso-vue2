1d844b503a8683aeb0720e07bb94a9d60c9676b8
Dygerydoo <caserofrancisco@gmail.com>
2017-07-30 13:06:42 +0200

    Props validation lesson

---
+++ lessons/props/props-validation/src/components/ChildTwo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="notification is-primary">
+    <h1 class="title">Componente hijo 2</h1>
+    Primar lorem ipsum dolor sit amet, consectetur
+    adipiscing elit lorem ipsum dolor sit amet,
+    consectetur adipiscing elit
+
+    <ol>
+      <li v-for="color in arrayReverse">{{color}}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    colors: {
+      type: Array,
+      default: function() {
+        return ['A', 'B']
+      },
+    },
+  },
+  computed: {
+    arrayReverse() {
+      return this.colors.reverse();
+    }
+  }
+}
+</script>
+
+<style>
+ol {
+  margin: 1em;
+}
+</style>
---
+++ lessons/props/props-validation/src/components/Parent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="parent">
+    <h1 class="title">{{title}}</h1>
+    <app-comment-block :title="title"></app-comment-block>
+    <app-info :colors="colors"></app-info>
+    <button class="button is-danger" @click="newTitle">Cambiar título</button>
+  </div>
+</template>
+
+<script>
+import AppCommentBlock from './ChildOne.vue';
+import AppInfo from './ChildTwo.vue';
+
+export default {
+  data() {
+    return {
+      title: 'Título del padre',
+      colors: ['Naranja', 'Azul', 'Turquesa', 'Granate'],
+    };
+  },
+  methods: {
+    newTitle() {
+      this.title = 'Título modificado';
+    }
+  },
+  components: {
+    AppCommentBlock,
+    AppInfo,
+  }
+}
+</script>
+
+<style scoped>
+#parent {
+  width: 80%;
+  margin: 2em auto;
+  background: #8655b7;
+  padding: 3em;
+}
+
+.title {
+  color: white;
+}
+</style>
