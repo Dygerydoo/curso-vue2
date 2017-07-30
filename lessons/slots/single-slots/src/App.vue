f530e7a69b131b2c6c4880536ce6708197c6e186
Dygerydoo <caserofrancisco@gmail.com>
2017-07-30 21:31:44 +0200

    single slots changes

---
+++ lessons/slots/single-slots/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <song-list>
+      <button class="button is-primary">Botón</button>
+    </song-list>
+  </div>
+</template>
+
+<script>
+import SongList from './components/SongList.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SongList,
+  }
+}
+</script>
+
+<style>
+</style>
