ac62269a6b9d3ff3fffafae7eb89a7a36b32006a
Dygerydoo <caserofrancisco@gmail.com>
2017-07-30 17:05:34 +0200

    add custom events emit lesson

---
+++ lessons/custom-events/emit-events/src/components/Parent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="parent">
+    <h1 class="title">{{title}}</h1>
+    <app-comment-block v-if="show" @hide="show = false" :title="title"></app-comment-block>
+    <button v-if="!show" class="button is-danger" @click="showBlock">Mostrar de nuevo</button>
+  </div>
+</template>
+
+<script>
+import AppCommentBlock from './ChildOne.vue';
+
+export default {
+  data() {
+    return {
+      show: true,
+      title: 'Título',
+    };
+  },
+  methods: {
+    showBlock() {
+      this.show = true;
+    },
+  },
+  components: {
+    AppCommentBlock,
+  }
+}
+</script>
+
+<style scoped>
+#parent {
+  width: 80%;
+  margin: 2em auto;
+  background: #8655b7;
+  padding: 3em;
+}
+
+.title {
+  color: white;
+}
+</style>
