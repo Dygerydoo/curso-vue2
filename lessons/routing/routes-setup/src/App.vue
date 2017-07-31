15adce73f00fa326318776aac5673bf083d214c6
Dygerydoo <fran@beroomers.com>
2017-07-31 22:20:45 +0200

    remove nav from routes lesson

--- lessons/routing/routes-setup/src/App.vue
+++ lessons/routing/routes-setup/src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <nav class="navbar">
-      <ul class="navbar-list">
-        <li class="navbar-list-item">Inicio</li>
-        <li class="navbar-list-item">Contacto</li>
-        <li class="navbar-list-item">Usuario</li>
-      </ul>
-    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -17,19 +10,4 @@ export default {
 </script>
 
 <style>
-.navbar {
-  display: flex;
-  justify-content: flex-start;
-  border-bottom: 1px solid #ddd;
-  margin-bottom: 1em;
-}
-
-.navbar-list {
-  display: flex;
-  flex-direction: row;
-}
-
-.navbar-list-item {
-  margin: 1em;
-}
 </style>
