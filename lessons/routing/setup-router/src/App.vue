bcdaa338fa37491132ad243eacd1ef09bb58a301
Dygerydoo <caserofrancisco@gmail.com>
2017-07-31 16:28:28 +0200

    added setup-router config

---
+++ lessons/routing/setup-router/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <nav class="navbar">
+      <ul class="navbar-list">
+        <li class="navbar-list-item">Inicio</li>
+        <li class="navbar-list-item">Contacto</li>
+        <li class="navbar-list-item">Usuario</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 1em;
+}
+
+.navbar-list {
+  display: flex;
+  flex-direction: row;
+}
+
+.navbar-list-item {
+  margin: 1em;
+}
+</style>
---
+++ lessons/routing/setup-router/src/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="">
+    <h1>Hola, estás en la home</h1>
+    <p>¿A que sección quieres ir?</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
