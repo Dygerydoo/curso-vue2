bfead82d361061cf961384e659e0582d6c4db76d
Dygerydoo <fran@beroomers.com>
2017-07-31 18:26:18 +0200

    routes-setup

---
+++ lessons/routing/routes-setup/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <nav class="navbar">
+      <ul class="navbar-list">
+        <li class="navbar-list-item">Inicio</li>
+        <li class="navbar-list-item">Contacto</li>
+        <li class="navbar-list-item">Usuario</li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 1em;
+}
+
+.navbar-list {
+  display: flex;
+  flex-direction: row;
+}
+
+.navbar-list-item {
+  margin: 1em;
+}
+</style>
---
+++ lessons/routing/routes-setup/src/components/userPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="column is-half is-offset-one-quarter">
+    <h1 class="title is-1 has-text-centered">Bienvenido a tu página de usuario</h1>
+    <div class="box">
+      <article class="media">
+        <figure class="media-left">
+          <p class="image is-64x64">
+            <img src="http://bulma.io/images/placeholders/128x128.png">
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+              <br>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
