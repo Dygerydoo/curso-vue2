c0132b66afe3644e1151df018fd3413250647425
Dygerydoo <fran@beroomers.com>
2017-07-31 22:19:12 +0200

    router-links

---
+++ lessons/routing/router-links/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <app-navigation></app-navigation>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AppNavigation from './components/Navigation.vue';
+export default {
+  components: {
+    AppNavigation,
+  }
+}
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 1em;
+}
+
+.navbar-list {
+  display: flex;
+  flex-direction: row;
+}
+
+.navbar-list-item {
+  margin: 1em;
+}
+</style>
---
+++ lessons/routing/router-links/src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li class="navbar-list-item">Inicio</li>
+      <li class="navbar-list-item">Contacto</li>
+      <li class="navbar-list-item">Usuario</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
