b8d77459a664e8b7cd08f146387e9a36c7c1af90
Dygerydoo <fran@beroomers.com>
2017-08-08 10:01:56 +0200

    code-navigation

---
+++ lessons/routing/code-navigation/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box column is-half is-offset-3">
+    <h1 class="title is-1">Hola, estás en la home</h1>
+    <p>Navega a cualquier sección</p>
+    <hr>
+    <p>Si tienes cualquier duda</p>
+    <br>
+    <button class="button is-primary"
+            @click="contactUs">Contacta con nosotros</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    contactUs() {
+      this.$router.push('/contacto');
+    }
+  }
+}
+</script>
+
+<style>
+</style>
