6130a2f5773acbc71bafc7365af88a2fac3028cc
Dygerydoo <fran@beroomers.com>
2017-08-08 12:45:45 +0200

    route-params

---
+++ lessons/routing/route-params/src/components/Contact.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="column is-half">
+    <h1 class="title is-1">Página de contacto</h1>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Text input">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Text input" value="">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Text input" value=""></textarea>
+      </div>
+    </div>
+
+    <div class="control">
+      <button class="button is-primary">Submit</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ lessons/routing/route-params/src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/home.vue';
+import User from './components/userPage.vue';
+import Contact from './components/contact.vue';
+
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/usuario/:userName', component: User },
+  { path: '/contacto', component: Contact }
+];
