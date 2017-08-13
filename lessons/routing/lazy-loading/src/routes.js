d4d5c86b39ab31c048f9b5b24b9e0fbe5167d2f3
Dygerydoo <fran@beroomers.com>
2017-08-14 01:12:35 +0200

    lazy loading

---
+++ lessons/routing/lazy-loading/src/routes.js
@@ -0,0 +1,22 @@
+import Home from './components/Home.vue';
+// import User from './components/user/User.vue';
+import UserPage from './components/user/UserPage.vue';
+import UserList from './components/user/UserList.vue';
+import UserDelete from './components/user/UserDelete.vue';
+import Contact from './components/Contact.vue';
+
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  }, 'userGroup');
+}
+
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/usuario', component: User, children: [
+      { path: '', component: UserList },
+      { path: ':userName', component: UserPage },
+      { path: ':userName/delete', component: UserDelete, name: 'DeleteUser' },
+  ] },
+  { path: '/contacto', component: Contact }
+];
