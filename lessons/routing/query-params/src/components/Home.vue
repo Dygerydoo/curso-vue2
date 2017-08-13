bcff7a123db97dee95e9ec53e9501bf0e099d09a
Dygerydoo <fran@beroomers.com>
2017-08-13 18:10:08 +0200

    query params

---
+++ lessons/routing/query-params/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box column is-half is-offset-3">
+    <h1 class="title is-1">Hola, estás en la home</h1>
+    <p>Navega a cualquier sección</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ lessons/routing/query-params/src/components/user/UserList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="column is-half is-offset-one-quarter" style="background: #ddd; padding: 1em;">
+    <div class="tabs">
+      <ul>
+        <router-link to="/usuario/@jhonsmith" tag="li"><a>John</a></router-link>
+        <router-link to="/usuario/@evanyou" tag="li"><a>Evan</a></router-link>
+        <router-link to="/usuario/@francasero" tag="li"><a>Fran</a></router-link>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
