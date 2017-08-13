2f8b6a1f87e5b7332c6d4ee0e2fb4f39b5707992
Dygerydoo <fran@beroomers.com>
2017-08-13 23:36:38 +0200

    navigation guards

---
+++ lessons/routing/navigation-guards/src/components/user/User.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="column is-half is-offset-one-quarter" style="background: #bbb;">
+    <h1 class="title is-1">Página de usuario</h1>
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ lessons/routing/navigation-guards/src/components/user/UserDelete.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tile is-parent">
+      <article class="tile is-child notification is-danger">
+        <p class="title">¿Quieres eliminar el usuario {{ $route.params.userName }}?</p>
+        <p class="subtitle">No podrás deshacer esta acción</p>
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+        </div>
+      </article>
+      <button @click="saved = true">Guardar</button>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      saved: false,
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if(this.saved) {
+      next();
+    } else {
+      alert('No has guardado los cambios');
+      next(false);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
