69f59bea7b70efcccf8b27eee607b0c43aa63353
Dygerydoo <fran@beroomers.com>
2017-08-19 15:05:45 +0200

    setup-transitions

---
+++ lessons/transitions/setup-transitions/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <button class="button button-primary" @click="show = !show">Mostrar/Ocultar</button>
+    <br><br>
+    <transition>
+      <h1 v-if="show" class="title is-1">Transiciones con Vue</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      show: true,
+    }
+  }
+}
+</script>
+
+<style>
+
+#app {
+  padding: 1em;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
