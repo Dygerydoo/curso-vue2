215ee4e5ab1448bee0cbcecebc6d4a85a80fe956
Dygerydoo <fran@beroomers.com>
2017-08-20 17:22:44 +0200

    custom transitions

---
+++ lessons/transitions/custom-transitions/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <button class="button button-primary" @click="show = !show">Mostrar/Ocultar</button>
+    <br><br>
+    <transition name="my-custom-animation"
+                enter-active-class="animated rollIn"
+                leave-active-class="animated hinge"
+                >
+      <h1 v-if="show" class="title is-1">Transiciones con Vue</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      show: true,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
