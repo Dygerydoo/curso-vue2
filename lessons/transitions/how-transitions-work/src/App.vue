d50413f134c347f654e5f9006a3868b40b52f00b
Dygerydoo <fran@beroomers.com>
2017-08-20 15:59:06 +0200

    how transitions work

---
+++ lessons/transitions/how-transitions-work/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <button class="button button-primary" @click="show = !show">Mostrar/Ocultar</button>
+    <br><br>
+    <transition>
+      <h1 v-if="show" class="title is-1">Transiciones con Vue</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      show: true,
+    }
+  }
+}
+</script>
+
+<style>
+.v-enter {
+
+}
+
+.v-enter-active {
+
+}
+
+.v-leave {
+
+}
+
+.v-leave-active {
+
+}
+
+.v-leave-to{
+
+}
+
+#app {
+  padding: 1em;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
