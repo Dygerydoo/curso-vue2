564bd99b337bd9b1370c1f914ec4a81bd4a520b8
Dygerydoo <fran@beroomers.com>
2017-08-27 22:50:57 +0200

    mapping getters

---
+++ lessons/vuex/mapping-getters/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Ejemplo básico de Vuex</h1>
+    <p>El contador está a: {{ $store.state.counter }}</p>
+    <p> El contador sin dividir está a: {{ $store.state.counter }} y es {{ evenOrOdd }}</p>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/counter.vue'
+import { mapGetters } from 'vuex'
+
+console.log({key: 'value', mapGetters(['divideCounter', 'evenOrOdd'])});
+
+// Ejemplo de porque Spread Operator: Y esque devuelve {{lalala}} en lugar de { key: {}}
+// o sea objeto sin key, directamente objeto.
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'divideCounter',
+      'evenOrOdd'
+    ])
+  },
+  components: {
+    Counter,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lessons/vuex/mapping-getters/src/store/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    counter: 0,
+  },
+  getters: {
+    divideCounter: state => {
+      return state.counter / 2;
+    },
+    evenOrOdd: state => {
+      return state.counter % 2 === 0 ? 'par' : 'impar';
+    }
+  }
+});
