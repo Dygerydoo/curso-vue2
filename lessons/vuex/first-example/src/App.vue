f302acd74a2587f86fdaa84806124db6fe4e2aa2
Dygerydoo <fran@beroomers.com>
2017-08-27 17:08:32 +0200

    first-example

---
+++ lessons/vuex/first-example/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>Ejemplo básico de Vuex</h1>
+    <p>El contador está a: {{ counter }}</p>
+    <!-- <counter @increase="counter += $event"
+             @decrease="counter -= $event">
+    </counter> -->
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/counter.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  components: {
+    Counter,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lessons/vuex/first-example/src/components/counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="buttons">
+    <button @click="increaseCounter">Incrementar</button>
+    <button @click="decreaseCounter">Disminuir</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // Antes de aplicar Vuex
+    // increaseCounter() {
+    //   this.$emit('increase', 1);
+    // },
+    // decreaseCounter() {
+    //   this.$emit('decrease', 1);
+    // }
+
+    // Después de aplicar Vuex
+    increaseCounter() {
+      this.$store.state.counter++;
+    },
+    decreaseCounter() {
+      this.$store.state.counter--;
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
